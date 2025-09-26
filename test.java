e6d635d11ad406bb20dd3dab68bee29e9bd60de1
bsatyanarayanareddy <bsatyanarayanareddy_it221209@mgit.ac.in>
2025-09-26 22:35:14 +0530

    add files

---
+++ test.java
@@ -0,0 +1,15 @@
+class test
+{
+     int t=10;
+     public void  sum(){
+        System.out.println("sum is sum");
+        class Innertest {
+        
+            int te =20;
+            public testInnertest{
+            System.out.println("value is 20");
+            }
+        }
+     }
+
+}
