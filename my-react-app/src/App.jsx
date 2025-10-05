2d9fb92c7162373b388ebb8aab177044f0911e8f
bsatyanarayanareddy <bsatyanarayanareddy_it221209@mgit.ac.in>
2025-10-05 10:08:19 +0530

    add file

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import {Rod} from "./box";
+import Box from "./box";
+
+function App() {
+  return (
+    <>
+    <Box></Box>
+    <Rod></Rod>
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/box.jsx
@@ -0,0 +1,19 @@
+
+
+function Box() {
+    return (
+        <div className="box">
+            Box
+        </div>
+
+    );
+}
+function Rat(){
+    return (
+        <div className="rat">
+            Rat
+        </div>
+    );
+}
+export {Rat};
+export default Box;
